0cfbe5617a0a49d22cc6c85e0cec0a50ec50809e
johnny (jpixy) <13565121+jpixy@users.noreply.github.com>
2022-05-09 00:33:23 +0800

    finish #3 cpp way 1

--- 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.h
+++ 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.h
@@ -1,43 +1,50 @@
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(): val(0), next(nullptr) {}
-    ListNode(int x): val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next): val(x), next(next) {}
-};
-
+#include <string>
+using std::string;
+#include <unordered_map>
+using std::unordered_map;
+#include <algorithm>
+using std::max;
 
 class Solution {
   public:
-    ListNode* addTwoNumber(ListNode* l1, ListNode* l2) {
-        ListNode* head = new ListNode(0);
-        ListNode* current = head;
-        int n1 = 0;
-        int n2 = 0;
-        int carry = 0;
-
-        /* DO NOT forget add carry != 0, the last carry */
-        while (l1 || l2 || carry) {
-            if (l1) {
-                n1 = l1->val;
-                l1 = l1->next;
-            } else {
-                n1 = 0;
-            }
-
-            if (l2) {
-                n2 = l2->val;
-                l2 = l2->next;
-            } else {
-                n2 = 0;
-            }
-
-            current->next = new ListNode((n1 + n2 + carry) % 10);
-            current = current->next;
-            carry = (n1 + n2 + carry) / 10;
-
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() <= 1) {
+            return s.size();
         }
-
-        return head->next;
+        int max_length = 0;
+        unordered_map<char, int> tmpMap;
+        for (int left_cursor = 0; left_cursor < s.size(); left_cursor++) {
+            tmpMap.clear();
+            for (int right_cursor = 0; right_cursor < s.size(); right_cursor++) {
+                if (right_cursor >= left_cursor) {
+                    /* Note, if the substring without repeating chars is end with tail, */
+                    /* then we need take care of that] */
+                    auto found = tmpMap.find(s[right_cursor]);
+                    if (found != tmpMap.end() || right_cursor == s.size()) {
+                        break;
+                    }
+                    tmpMap[s[right_cursor]] = right_cursor;
+                }
+            }
+            max_length = max(max_length, tmpMap.size());
+        }
+        return max_length;
     }
+
+  private:
+    int max(int x, int y) {
+        if (x > y) {
+            return x;
+        }
+        return y;
+    }
+
+  private:
+    void printMap(unordered_map<char, int> t_map) {
+        for (auto kv : t_map) {
+            std::cout << kv.first << ":" << kv.second << "->";
+        }
+        std::cout << "\b\b" << std::endl;
+    }
+
 };
--- 3. Longest Substring Without Repeating Characters/test.cpp
+++ 3. Longest Substring Without Repeating Characters/test.cpp
@@ -1,31 +1,17 @@
 /*turn off the assert*/
 /* #define NDEBUG */
 #include <iostream>
+#include <string>
 #include <cassert>
-#include "add_two_numbers.h"
+#include "longest_substring_without_repeating_characters.h"
 
-ListNode* create_linkedlist(std::initializer_list<int> lst) {
-    auto iter = lst.begin();
-    ListNode* head = lst.size() ? new ListNode(*iter++) : NULL;
-    for (ListNode* cur = head; iter != lst.end(); cur = cur->next)
-        cur->next = new ListNode(*iter++);
-    return head;
-}
-
-int main() {
+int main()
+{
     Solution s;
-
-    ListNode *l1 = create_linkedlist({9, 9, 9, 9});
-    ListNode *l2 = create_linkedlist({9, 9});
-    ListNode *l3 = create_linkedlist({8, 9, 0, 0, 1});
-
-    ListNode *ret = s.addTwoNumber(l1, l2);
-    for (ListNode *cur = ret; cur; cur = cur->next)
-        std::cout << cur->val << "->";
-    std::cout << "\b\b" << std::endl;
+    int result = s.lengthOfLongestSubstring("au");
 
     /* assert( (&ret ==  &l3)); */
-
+    std::cout << result << std::endl;
 
     return 0;
 }
